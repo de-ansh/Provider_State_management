f0cde428ac2c15a5d41180800bd4d0fdc6ac4c9a
Ashish <anshm9448@gmail.com>
2023-02-15 15:10:09 +0530

    PUsh

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -15,7 +16,7 @@ class _HomepageState extends State<Homepage> {
         title: const Text("Privider Home1"),
       ),
       body: Column(
-        children: [],
+        children: const [],
       ),
     );
   }
@@ -24,8 +25,21 @@ class _HomepageState extends State<Homepage> {
 class BreadCrumb {
   bool isActive;
   final String name;
-  BreadCrumb({required this.isActive, required this.name});
+  final String uuid;
+  BreadCrumb({required this.isActive, required this.name})
+      : uuid = const Uuid().v4();
+
   void activate() {
     isActive = true;
   }
+
+  @override
+  bool operator ==(covariant BreadCrumb other) => uuid == other.uuid;
+  
+  @override
+  // TODO: implement hashCode
+  int get hashCode => uuid.hashCode;
+
+  String get title=> name + (isActive? '>' :''  );
+  
 }
